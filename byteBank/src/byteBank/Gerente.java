698b8312e870c35f6c7f1c7c2c55456676144f4a
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-03-16 07:53:24 -0300

    Update Class Gerente
    *criação do metodo autentica()

--- byteBank/src/byteBank/Gerente.java
+++ byteBank/src/byteBank/Gerente.java
@@ -2,6 +2,14 @@ package byteBank;
 
 public class Gerente extends Funcionario {
 	
+	private int senha;
 	
+	public boolean autentica(int senha) {
+	    if (this.senha == senha) {
+	        return true;
+	    } else {
+	        return false;
+	    }
+	}
 
 }
