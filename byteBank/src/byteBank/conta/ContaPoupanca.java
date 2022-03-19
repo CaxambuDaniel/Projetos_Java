4008dcaf86832b2b173580b8f9e7c02b6f8bd8e4
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-03-19 10:51:58 -0300

    Update Class Conta
    *definindo método deposita() como abstrato
    *alterando o modificador de acesso do atributo saldo para protected
    *implementando método deposita nas classes filhas

--- byteBank/src/byteBank/conta/ContaPoupanca.java
+++ byteBank/src/byteBank/conta/ContaPoupanca.java
@@ -1,9 +1,16 @@
 package byteBank.conta;
 
-public class ContaPoupanca extends ContaCorrente {
+public class ContaPoupanca extends Conta {
 	
 	public ContaPoupanca(int agencia, int numero) {
 		super(agencia, numero);
 	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;		
+	}
+	
+	
 
 }
