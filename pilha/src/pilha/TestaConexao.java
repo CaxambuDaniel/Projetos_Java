2a611c7ea6cd887c0ef782195bfb33b885fc39fb
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-03-28 23:50:00 -0300

    Project pilha - Update Class TestaConexao
    *Simplificando try catch finaly

--- pilha/src/pilha/TestaConexao.java
+++ pilha/src/pilha/TestaConexao.java
@@ -4,14 +4,11 @@ public class TestaConexao {
 
 	public static void main(String[] args) {
 		
-		Conexao con = null;		
-		try {
-			con = new Conexao();
+			
+		try (Conexao con = new Conexao()) {
 			con.leDados();
 		} catch (Exception e) {
 			System.out.println("Deu Ruim na conexão");
-		} finally {
-			con.fecha();
 		}
 		
 
