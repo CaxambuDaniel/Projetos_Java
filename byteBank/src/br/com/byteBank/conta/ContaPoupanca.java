80ec95d9fc14b761ea4e7835f23d57a221d2397f
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-04-08 05:26:14 -0300

    update ContaPoupanca
    Sobrescrição do método ToString, da classe Conta.

--- byteBank/src/br/com/byteBank/conta/ContaPoupanca.java
+++ byteBank/src/br/com/byteBank/conta/ContaPoupanca.java
@@ -10,6 +10,14 @@ public class ContaPoupanca extends Conta {
 	public void deposita(double valor) {
 		super.saldo += valor;		
 	}
+	/**
+	 * Sobrescrição do método ToString, da classe Conta.
+	 * O intuito da sobrescrição é deixar mais clara a mensagem apresentada ao imprimirmos na tela a referencia de um objeto ContaPoupanca
+	 */
+	@Override
+	public String toString() {
+		return "Conta Poupança, " + super.toString();
+	}
 	
 	
 
