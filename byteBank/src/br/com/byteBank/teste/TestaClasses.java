0ddfb51764c0f848f36f53d99e7c2fde2a367d82
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2022-04-12 07:07:40 -0300

    Create package guardaConta

--- byteBank/src/br/com/byteBank/teste/TestaClasses.java
+++ byteBank/src/br/com/byteBank/teste/TestaClasses.java
@@ -1,5 +1,7 @@
 package br.com.byteBank.teste;
 
+import java.util.Iterator;
+
 import br.com.byteBank.conta.Conta;
 import br.com.byteBank.conta.ContaCorrente;
 import br.com.byteBank.conta.ContaPoupanca;
@@ -8,11 +10,28 @@ import br.com.byteBank.exceptions.SaldoInsuficienteException;
 public class TestaClasses {
 	public static void main(String[] args) {
 
-		Conta cc = new ContaCorrente(3347, 321456);
-		Conta cp = new ContaPoupanca(7745, 112445);
+//		Conta cc = new ContaCorrente(3347, 321456);
+//		Conta cp = new ContaPoupanca(7745, 112445);
+//		
+//		System.out.println(cc);
+//		System.out.println(cp);
 		
-		System.out.println(cc);
-		System.out.println(cp);
+		//criando uym array de referencia
+		ContaCorrente [] contas = new ContaCorrente[5];
+		ContaCorrente conta1 = new ContaCorrente(124, 132);
+		ContaCorrente conta2 = new ContaCorrente(142, 123);
+		
+		contas[0]= conta1;
+		contas[1]= conta2;
+		
+		System.out.println(contas[0].getAgencia());
+		
+		for (int i = 0; i < args.length; i++ ) {
+			System.out.println(args[i]);
+			
+		}
+		
+		
 		
 		
 	}
